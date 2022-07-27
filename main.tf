611e0217ec63e6f7e8305d9025ffc4e5c29efd8a
JorgeLuisPenaranda <jorge.penaranda.arandia@gmail.com>
2022-07-26 19:54:03 -0700

    GHA CI 08

--- main.tf
+++ main.tf
@@ -41,8 +41,40 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name = "jpenaranda"
+  vpc_security_group_ids = [vpc_security_group.fslsg.id]
 
   tags = {
     Name = "FSL test"
+  }
+}
+
+resource "aws_security_group" "fslsg" {
+  name        = "allow_tls"
+  
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
